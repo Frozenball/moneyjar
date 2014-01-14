cf350eaa763a566171faa95a4eda392b03163475
Teemu <teemu.on.ihminen@gmail.com>
2014-01-14 22:23:18 +0200

    Added scope

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ console.log(settings);
 everyauth.google
   .appId(settings.google.appId)
   .appSecret(settings.google.appSecret)
+  .scope('https://www.google.com/m8/feeds')
   .handleAuthCallbackError( function (req, res) {
     // If a user denies your app, Google will redirect the user to
     // /auth/facebook/callback?error=access_denied
